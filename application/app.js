9f9b4b7cf88cbe4ed183029a9e9f1e08ff03c01f
xfloooo <xfloooo1@gmail.com>
2018-03-15 23:53:45 +0300

    move creating store to index

--- application/app.js
+++ application/app.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { Provider, observer } from 'mobx-react';
 import { ShellBase } from './shell';
-import  {RootStore} from './stores/root.store'
-import { createBrowserHistory } from 'history';
-import { HistoryAdapter } from 'mobx-state-router';
 
-const history = createBrowserHistory();
-const rootStore = new RootStore();
-const historyAdapter = new HistoryAdapter(rootStore.routerStore, history);
-historyAdapter.observeRouterStateChanges();
 
 // const staticAdapter = new StaticAdapter(rootStore.routerStore, '/items');
 // staticAdapter.preload();
-@observer
 class App extends React.Component {
     render() {
 
         return (
-                <Provider rootStore={rootStore}>
+                <Provider rootStore={this.props.rootStore}>
                     <ShellBase />
                 </Provider>
         );
--- application/index.js
+++ application/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom'
 // import { hot } from 'react-hot-loader'
 import App from './app'
 
+import  {RootStore} from './stores/root.store'
+import { createBrowserHistory } from 'history';
+import { HistoryAdapter } from 'mobx-state-router';
+
+const history = createBrowserHistory();
+const rootStore = new RootStore();
+const historyAdapter = new HistoryAdapter(rootStore.routerStore, history);
+historyAdapter.observeRouterStateChanges();
+
 
 ReactDOM.render(
-    <App />,
+    <App rootStore={rootStore}/>,
     document.getElementById("root")
 );
 
